6952cf2a7c7744367aac02775ac5ca738ae189e9
Daniel Bernstein <d.bernstein@gothaer-digital.de>
2023-11-13 15:00:18 +0100

    add pictures methods

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,8 +11,23 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {
-    return;
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 600,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
   }
 
   @override
@@ -24,10 +42,39 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
+      child: _selectedImage == null
+          ? TextButton.icon(
+              onPressed: _takePicture,
+              icon: const Icon(Icons.camera),
+              label: const Text('Take Picture'),
+            )
+          : ImagePreview(
+              image: _selectedImage!,
+              onTap: _takePicture,
+            ),
+    );
+  }
+}
+
+class ImagePreview extends StatelessWidget {
+  final File image;
+  final void Function() onTap;
+
+  const ImagePreview({
+    required this.image,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.file(
+        image,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
       ),
     );
   }
